011642efbebf10d84ce9ee909d2e846c88552c3d
Elsa224 <elsazougouri224@gmail.com>
2020-11-29 20:45:09 +0000

    edited existing file

--- Helloworld/exercice.c
+++ Helloworld/exercice.c
@@ -4,3 +4,7 @@ int main(int argc, char const *argv[])
     printf("Hello all of you !! \n");
     return 0;
 }
+
+
+/* echo int main(int argc, char const *argv[]){//Déclaration des variables printf("Hello all of you !! \n");return 0;} > test.c
+*/
