876de9c04ec46c62b492403e2147273e6924ecd4
Pablo_E <tatipaulmarie@gmail.com>
2021-05-28 14:20:57 +0000

    Debugging with superPablo_E

--- atelierC_algo/algoPile.c
+++ atelierC_algo/algoPile.c
@@ -8,54 +8,69 @@
 
 #include "algoPile.h"
 
-int main(int argc, char const *argv[])
-{
-    PILE p;
-    int option = 1, choice, element;
+PILE p;
 
+int main( int argc, char const *argv[] )
+{
+    int option = 1, choice, element;
+    
     //Test des operations
-    initPILE(p);    //p.sommet = -1 on suppose pile vide au debut
+    initPILE( &p );    //p.sommet = -1 on suppose pile vide au debut    
 
     //Menu 
     printf("\n\tImplementation d'une pile");
-    while (option) 
+    while ( option ) 
     {
-        system("cls");
+        OS == 32 || OS == 64 ? system("cls") : system("clear");
         printf("\nMenu principal");
         printf("\n\t1.Empiler \n\t2.Depiler \n\t3.Afficher \n\t4.Verifier si la pile est vide \n\t5.Verifier si la pile est pleine \n\t6.exit");
         printf("\n\nEntrez votre choix: ");
         scanf("%d", &choice);
-        switch (choice) 
+        switch ( choice ) 
         {
             case 1:
                 printf("\nEntrer L'element a empiler: ");
                 scanf("%d", &element);
-                EMPILER(p, element);
+                EMPILER( &p, element );
             break;
 
             case 2:
-                element = DEPILER(p);
-                printf("\nL'element depile est %d", element);
+                element = DEPILER( &p );
+                element == -1 ? printf("") :  printf("\nL'element depile est %d", element);
             break;
 
             case 3:
-                printf("sommet = %d", p.sommet);
-                afficherPILE(p);
-
+                afficherPILE( &p );
             break;
 
             case 4:
-                PILEVIDE(p);
+                if ( PILEVIDE( &p ) )
+                    printf(" Pile Vide  ");
+                else 
+                    printf(" Pile non vide  ");
+
             break;
 
             case 5:
-                PILEPLEINE(p);
+                if ( PILEPLEINE( &p ) )
+                    printf(" Pile pleine  ");
+                else 
+                    printf(" Pile non pleine  ");
+            break;
 
             case 6:
-                exit(0);
+                exit( 0 );
+            break;
+
+            default:
+                OS == 32 || OS == 64 ? system("cls") : system("clear");
+                printf("\nMenu principal");
+                printf("\n\t1.Empiler \n\t2.Depiler \n\t3.Afficher \n\t4.Verifier si la pile est vide \n\t5.Verifier si la pile est pleine \n\t6.exit");
+                printf("\n\nEntrez votre choix: ");
+                scanf("%d", &choice);       
+            break;
         }
         printf("\nVoulez-vous continuer ? Tapez 0(Non) ou 1(Oui) : ");
         scanf("%d", &option);
     }
-    return 0;
-}
+    return 0;}
--- atelierC_algo/algoPile.h
+++ atelierC_algo/algoPile.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "os.h"
 
 /**** IMPLEMENTATION DES OPERATIONS SUR LA PILE*********/
 
@@ -17,27 +18,27 @@ typedef struct pile PILE;
 
 
 /****************** PROTOTYPES ****************/
-void initPILE(PILE P);
-void EMPILER(PILE P, int a);
-int DEPILER(PILE P);
-bool PILEVIDE(PILE P);
-bool PILEPLEINE(PILE P);
-void afficherPILE(PILE P);
+void initPILE(PILE *P);
+void EMPILER(PILE *P, int a);
+int DEPILER(PILE *P);
+bool PILEVIDE(PILE *P);
+bool PILEPLEINE(PILE *P);
+void afficherPILE(PILE *P);
 /***********************************************/
 
 //Initialisation de la pile
-void initPILE(PILE P)
+void initPILE(PILE *P)
 {
-    P.sommet = -1;
+    P->sommet = -1;
 }
 
 //Procedure EMPILER()
-void EMPILER(PILE P, int a)
+void EMPILER(PILE *P, int a)
 {
     if(!PILEPLEINE(P))
     {
-        P.sommet = P.sommet + 1;
-        P.tab[P.sommet] = a;
+        P->sommet = P->sommet + 1;
+        P->tab[P->sommet] = a;
     }
     else
         printf("La pile est pleine! Plus d'ajout possible");
@@ -45,26 +46,35 @@ void EMPILER(PILE P, int a)
 }
 
 //Fonction DEPILER()
-int DEPILER(PILE P)
+int DEPILER(PILE *P)
 {
-    int nombre = P.tab[P.sommet];
-    P.sommet--;
+    int nombre;
+    if( ( PILEVIDE( P ) ) )
+    {
+        printf("Pile vide ! Veuillez empiler au moins 1 element avant de depiler ! ");
+        nombre = -1;
+    }
+    else
+    {
+        nombre = P->tab[P->sommet];
+        P->sommet--;
+    }
     return nombre;
 }
 
 //Fonction PILEVIDE()
-bool PILEVIDE(PILE P)
+bool PILEVIDE(PILE *P)
 {
-    if(P.sommet == -1)
+    if(P->sommet == -1)
         return true;
     else
         return false;
 }
 
 //Fonction PILEPLEINE()
-bool PILEPLEINE(PILE P)
+bool PILEPLEINE(PILE *P)
 {
-    if (P.sommet == taille)
+    if (P->sommet == taille)
         return true;  
     else
         return false;
@@ -72,17 +82,14 @@ bool PILEPLEINE(PILE P)
 }
 
 //Fonction afficherPILE
-void afficherPILE(PILE P)
+void afficherPILE(PILE *P)
 {
-    if(PILEVIDE(P))
+    if( PILEVIDE( P ) )
         printf("\nLa pile est vide !");
     else
     {
-        printf("|");
-        for (int i = P.sommet; i >= 0; i--) //l'affichage de la PILE est contraire à l'affichage d'un tableau normal
-        {
-           printf(" %d |", P.tab[i]);
-        }  
+        for (int i = P->sommet; i >= 0; i--) //l'affichage de la PILE est contraire à l'affichage d'un tableau normal
+           printf(" | %d |\n", P->tab[i]);
     }
 
 }
