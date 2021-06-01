060acadba66309de37bb833bb14a90c0397846ed
Pablo_E <tatipaulmarie@gmail.com>
2021-06-01 10:56:00 +0000

    Retrait variables non utilisées

--- atelierC_algo/algoListeChainee.c
+++ atelierC_algo/algoListeChainee.c
@@ -26,7 +26,6 @@ int main( int argc, char const *argv[] )
     pabloListe = &pabloFirstElement; // La liste dont le premier élément ( son succ ) est NULL donc liste vide
     int number;
     listeVide( pabloListe ) ? printf( "Liste vide\n" ) : printf( "Liste non vide\n" ); // après initialisation si nous sommes en fin de liste alors la liste est vide
-    // pabloInsererLISTE( pabloListe );
     pabloMultiInsererLISTE( 2 , pabloListe );
     printf( "Derniere cle entree est : %d\n", pabloListe->suivant->valeur );
     listeVide( pabloListe ) ? printf( "Liste non vide\n" )  : printf( "Liste vide\n" ); // après initialisation si nous sommes en fin de liste alors la liste est vide
