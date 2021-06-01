097d168d71c58554cf7e89367b0576a5fe7ef48d
Pablo_E <tatipaulmarie@gmail.com>
2021-06-01 10:47:29 +0000

    pabloVersion

--- atelierC_algo/algoListeChainee.c
+++ atelierC_algo/algoListeChainee.c
@@ -17,7 +17,22 @@ int main( int argc, char const *argv[] )
     //Declaration et allocation d'un element cellule
     LISTE list;
     list = malloc( sizeof( struct cellule ) );
-    tete = &list; //tete contient l'adresse du premier element de la liste
+    tete = list; //tete contient l'adresse du premier element de la liste
+
+
+    LISTE pabloListe;
+    elementListe pabloFirstElement, anotherOne;
+    pabloFirstElement.suivant = NULL; // Premier élément de la liste 
+    pabloListe = &pabloFirstElement; // La liste dont le premier élément ( son succ ) est NULL donc liste vide
+    int number;
+    tete = NULL; // Initialisation de la liste
+    listeVide( pabloListe ) ? printf( "Liste vide\n" ) : printf( "Liste non vide\n" ); // après initialisation si nous sommes en fin de liste alors la liste est vide
+    // pabloInsererLISTE( pabloListe );
+    pabloMultiInsererLISTE( 2 , pabloListe );
+    printf( "Derniere cle entree est : %d\n", pabloListe->suivant->valeur );
+    listeVide( pabloListe ) ? printf( "Liste non vide\n" )  : printf( "Liste vide\n" ); // après initialisation si nous sommes en fin de liste alors la liste est vide
+
+
 
 
     return 0;
--- atelierC_algo/algoListeChainee.h
+++ atelierC_algo/algoListeChainee.h
@@ -8,7 +8,8 @@
  * qui est un pointeur sur une cellule.
  * Donc le type LISTE (la liste elle-meme) est un pointeur sur cette suite de cellules
  * 
-/*********************************************************/
+ ******************************************************************************
+*/
 
 
 //Declaration du type cellule
@@ -17,7 +18,7 @@ struct cellule
     int valeur;
     struct cellule *suivant;
 };
-typedef struct cellule *LISTE;
+typedef struct cellule * LISTE;
 
 //Tete de liste
 LISTE tete;
@@ -29,7 +30,7 @@ LISTE insererLISTE( int num, LISTE Q )
 {
     LISTE L;
     L = malloc( sizeof( struct cellule ) ); //allouer(L)
-    tete = &L;  //Comme on insere en tete, tete doit toujours pointer sur l'element qu'on insere
+    tete = L;  //Comme on insere en tete, tete doit toujours pointer sur l'element qu'on insere
     L->valeur = num;
     L->suivant = Q; //car le suivant pointe sur un element de type cellule (donc cellule)
     return L;
@@ -38,3 +39,26 @@ LISTE insererLISTE( int num, LISTE Q )
 //je m'arrete la pour ce soir
 //je me demande meme si c'est juste
 //il va regarder aujourd'hui
+
+bool listeVide ( LISTE Q ) {
+    return Q->suivant == NULL ;
+}
+
+typedef struct cellule elementListe;
+
+//Fonction insererLISTE() en tete de liste : @superPablo_E
+void pabloInsererLISTE( LISTE Q )
+{
+    //Allocation d'un nouvel élément, je te laisse faire la version malloc
+    elementListe nouvelElement;
+    printf( "Veuillez entrer la cle de votre element : " );
+    scanf("%d", &nouvelElement.valeur);
+    Q->suivant = &nouvelElement; // succ [ pabloFirstElement ] pointe sur le nouvel élément
+    Q = Q->suivant; // pabloListe à son tour pointe sur le nouvel élément
+    Q->suivant = NULL;
+}
+
+void pabloMultiInsererLISTE( int nbElementToInsert, LISTE Q ) {
+    for ( int i = 0; i< nbElementToInsert; i++ )
+        pabloInsererLISTE( Q );
+}
